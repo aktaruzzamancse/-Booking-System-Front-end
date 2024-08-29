7f99ee7aa4d020748a2e7b7a60d39cf65c7438e9
Mohammad Aktaruzzaman <aktaruzzamancse@gmail.com>
2024-08-29 19:56:51 +0600

    Redux init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-
+import { useDispatch,useSelector } from "react-redux"
+import {increment, decrement  } from "./redux/features/counterSlice"
 function App() {
- 
-
+ const dispatch = useDispatch();
+ const count = useSelector((state) => state.counter.count)
   return (
     <>
-      <h1>Allhumdulliah</h1>
+      <h1 onClick={()=> dispatch(increment())}>Allhumdulliah </h1>
+      <p> {count}</p>
     </>
   )
 }
